c4d8603740abb677ae254fc759537bdf028eb576
Corneilious Eanes <whizvox0@gmail.com>
2021-02-18 09:57:31 -0800

    Add file I/O-based init method to AddressBookApplication

--- src/main/java/AddressBookApplication.java
+++ src/main/java/AddressBookApplication.java
@@ -1,7 +1,37 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class AddressBookApplication {
 
-  public static void main(String[] args) {
+  private static AddressBook ab;
 
+  public static void main(String[] args) throws Exception{
+    ab = new AddressBook();
+    init("AddressInputDataFile.txt");
+  }
+
+  public static void init(String fileName) throws IOException {
+    try (FileInputStream in = new FileInputStream(fileName)) {
+      Scanner scanner = new Scanner(in);
+      while (scanner.hasNext()) {
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        String street = scanner.nextLine();
+        String city = scanner.nextLine();
+        String state = scanner.nextLine();
+        int zip = scanner.nextInt();
+        scanner.nextLine();
+        String email = scanner.nextLine();
+        String phone = scanner.nextLine();
+        Address address = new Address(street, city, state, zip);
+        AddressEntry entry = new AddressEntry(firstName, lastName, address, phone, email);
+        ab.add(entry);
+        System.out.println("Address entry added to book:");
+        System.out.println(entry);
+        System.out.println();
+      }
+    }
   }
 
 }
